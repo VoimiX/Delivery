71219bdd20c43590f1e445906716e92d283e36f2
VoimiX <sitnikov.alpha@mail.ru>
2024-05-26 22:24:21 +0300

    courier domain logic tests

---
+++ Tests/DeliveryApp.UnitTests/Domain/CourierTests.cs
@@ -0,0 +1,83 @@
+﻿using DeliveryApp.Core.Domain.CourierAggregate;
+using DeliveryApp.Core.Domain.OrderAggregate;
+using DeliveryApp.Core.Domain.SharedKernel;
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace DeliveryApp.UnitTests.Domain;
+
+public class CourierTests
+{
+    [Fact]
+    public void Test_courier_car_steps_distance_to_order()
+    {
+        Courier courier = new Courier(id: 7, "Vasya", new Car(id: 38, "Gazel"));        
+
+        Order order = new Order(
+            new Guid("11d34e42-5195-4dbc-a5a9-d7b45af19d78"),
+            new Location(3, 4),
+            new Weight(kilograms: 4)
+            );        
+
+        courier.StartWork();
+        courier.Status.Should().Be(CourierStatus.Ready);
+
+        courier.AssignOrder(order);
+        courier.Order.Should().Be(order);
+        courier.Status.Should().Be(CourierStatus.Busy);
+        courier.Order.Should().NotBeNull();
+        courier.Order.Status.Should().Be(OrderStatus.Assigned);
+
+        courier.StepsDistanceToOrder.Should().NotBeNull();
+        courier.StepsDistanceToOrder.Value.Should().BeApproximately(
+            (float)courier.Location.DistanceTo(order.Location) / courier.Transport.Speed, precision: 2);
+
+        courier.MakeStepToOrder();
+        courier.Status.Should().Be(CourierStatus.Busy);
+        courier.Location.Should().Be(new Location(3, 3));
+
+        courier.MakeStepToOrder();
+        courier.Status.Should().Be(CourierStatus.Ready);
+        courier.Order.Status.Should().Be(OrderStatus.Completed);
+        courier.Location.Should().Be(new Location(3, 4));
+    }
+
+    [Fact]
+    public void Test_courier_pedestrian_steps_distance_to_order()
+    {
+        Courier courier = new Courier(id: 7, "Vasya", new Pedestrian(id: 22, "Vasya"));
+
+        Order order = new Order(
+            new Guid("11d34e42-5195-4dbc-a5a9-d7b45af19d78"),
+            new Location(3, 2),
+            new Weight(kilograms: 1)
+            );
+
+        courier.StartWork();
+        courier.Status.Should().Be(CourierStatus.Ready);
+
+        courier.AssignOrder(order);
+        courier.Order.Should().Be(order);
+        courier.Status.Should().Be(CourierStatus.Busy);
+        courier.Order.Should().NotBeNull();
+        courier.Order.Status.Should().Be(OrderStatus.Assigned);
+
+        courier.StepsDistanceToOrder.Should().NotBeNull();
+        courier.StepsDistanceToOrder.Value.Should().BeApproximately(
+            (float)courier.Location.DistanceTo(order.Location) / courier.Transport.Speed, precision: 2);
+
+        courier.MakeStepToOrder();
+        courier.Status.Should().Be(CourierStatus.Busy);
+        courier.Location.Should().Be(new Location(2, 1));
+
+        courier.MakeStepToOrder();
+        courier.Status.Should().Be(CourierStatus.Busy);
+        courier.Location.Should().Be(new Location(3, 1));
+
+        courier.MakeStepToOrder();
+        courier.Status.Should().Be(CourierStatus.Ready);
+        courier.Order.Status.Should().Be(OrderStatus.Completed);
+        courier.Location.Should().Be(new Location(3, 2));
+    }
+}
