a5a082feb2cc91ec8cd76065f458190d7e1a93cd
VoimiX <sitnikov.alpha@mail.ru>
2024-05-26 17:10:52 +0300

    domain logic

--- DeliveryApp.Core/Domain/CourierAggregate/Courier.cs
+++ DeliveryApp.Core/Domain/CourierAggregate/Courier.cs
@@ -1,5 +1,6 @@
 ﻿using DeliveryApp.Core.Domain.Exceptions;
 using DeliveryApp.Core.Domain.SharedKernel;
+using DeliveryApp.Core.Domain.OrderAggregate;
 
 namespace DeliveryApp.Core.Domain.CourierAggregate
 {
@@ -21,14 +22,30 @@ namespace DeliveryApp.Core.Domain.CourierAggregate
         public Transport Transport { get; }
         public Location Location { get; }
         public CourierStatus Status { get; private set; }
+        public Order Order { get; private set; }
+        public float? StepsDistanceToOrder
+        {
+            get
+            {
+                if (Order == null) return null;
+
+                var steps = (float)Location.DistanceTo(Order.Location) / Transport.Speed;
+                return steps;
+            }
+        }
 
         public void SetStatus(CourierStatus status)
         {
             Status = status;
         }
 
-        public void StartWork()
+        public void StartWork(Order order)
         {
+            if (order == null) throw new ArgumentNullException(nameof(order));
+            if (order.Courier != this) throw new DeliveryException("Неправильный заказ, назначен другому курьеру.");
+
+            Order = order;
+
             if (Status == CourierStatus.Busy)
             {
                 throw new DeliveryException("Курьер занят, невозможно начать работу.");
