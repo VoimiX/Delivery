179630821fdc2d93dc0f772dc59d8260e2e88f00
VoimiX <sitnikov.alpha@mail.ru>
2024-05-26 22:33:57 +0300

    courier domain logic tests +weight exception

--- Tests/DeliveryApp.UnitTests/Domain/CourierTests.cs
+++ Tests/DeliveryApp.UnitTests/Domain/CourierTests.cs
@@ -4,6 +4,7 @@ using DeliveryApp.Core.Domain.SharedKernel;
 using System;
 using Xunit;
 using FluentAssertions;
+using DeliveryApp.Core.Domain.Exceptions;
 
 namespace DeliveryApp.UnitTests.Domain;
 
@@ -79,5 +80,23 @@ public class CourierTests
         courier.Status.Should().Be(CourierStatus.Ready);
         courier.Order.Status.Should().Be(OrderStatus.Completed);
         courier.Location.Should().Be(new Location(3, 2));
+    }
+
+    [Fact]
+    public void Test_fail_if_weight_exceeds()
+    {
+        Courier courier = new Courier(id: 7, "Vasya", new Pedestrian(id: 22, "Vasya"));
+
+        Order order = new Order(
+            new Guid("11d34e42-5195-4dbc-a5a9-d7b45af19d78"),
+            new Location(3, 2),
+            new Weight(kilograms: 10)
+            );
+
+        courier.StartWork();
+        courier.Status.Should().Be(CourierStatus.Ready);
+        
+        var deliveryException = Assert.Throws<DeliveryException>(() => courier.AssignOrder(order));
+        deliveryException.Message.Should().Be($"Транспорт курьера {courier.Transport} не позволяет по весу перевозить такой заказ.");
     }
 }
