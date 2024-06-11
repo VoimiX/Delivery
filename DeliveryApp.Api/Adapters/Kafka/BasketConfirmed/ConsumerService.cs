b09bd6435479e28514018e96f2301aa6a6a4d367
VoimiX <sitnikov.alpha@mail.ru>
2024-06-11 21:42:47 +0300

    module-8 init

---
+++ DeliveryApp.Api/Adapters/Kafka/BasketConfirmed/ConsumerService.cs
@@ -0,0 +1,68 @@
+﻿using Confluent.Kafka;
+using MediatR;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System.Runtime.Intrinsics.X86;
+
+namespace DeliveryApp.Api.Adapters.Kafka.BasketConfirmed;
+
+public class ConsumerService : BackgroundService
+{
+    private readonly IMediator _mediator;
+    private readonly IConsumer<Ignore, string> _consumer;
+
+    public ConsumerService(IMediator mediator, string messageBrokerHost)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        if (string.IsNullOrWhiteSpace(messageBrokerHost)) throw new ArgumentException(nameof(messageBrokerHost));
+
+        var consumerConfig = new ConsumerConfig
+        {
+            BootstrapServers = messageBrokerHost,
+            GroupId = "DeliveryConsumerGroup",
+            EnableAutoOffsetStore = false,
+            EnableAutoCommit = true,
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            EnablePartitionEof = true
+        };
+        _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        _consumer.Subscribe("basket.confirmed");
+        try
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+                var consumeResult = _consumer.Consume(cancellationToken);
+
+                if (consumeResult.IsPartitionEOF)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: {consumeResult.Message.Value}");
+                
+                //var basketConfirmedIntegrationEvent = JsonConvert.DeserializeObject<BasketConfirmedIntegrationEvent>(consumeResult.Message.Value);
+
+                //Тут ваш Use Case
+                //_mediator.pu
+
+                try
+                {
+                    _consumer.StoreOffset(consumeResult);
+                }
+                catch (KafkaException e)
+                {
+                    Console.WriteLine($"Store Offset error: {e.Error.Reason}");
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            _consumer.Close();
+        }
+    }
+}
