274a379fbbf18dbe2f9d58262de5ea5a673a778b
VoimiX <sitnikov.alpha@mail.ru>
2024-06-18 21:45:47 +0300

    module-9 SaveDomainEventsInOutboxEvents

--- DeliveryApp.Infrastructure/Adapters/Postgres/UnitOfWork.cs
+++ DeliveryApp.Infrastructure/Adapters/Postgres/UnitOfWork.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using DeliveryApp.Infrastructure.Adapters.Postgres.Entities;
+using MediatR;
+using Newtonsoft.Json;
 using Primitives;
 
 namespace DeliveryApp.Infrastructure.Adapters.Postgres;
@@ -17,7 +19,10 @@ public class UnitOfWork : IUnitOfWork
     public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
     {
         await _dbContext.SaveChangesAsync(cancellationToken);
+
         await PublishDomainEventsAsync();
+        await SaveDomainEventsInOutboxEventsAsync();
+
         return true;
     }
 
@@ -37,4 +42,36 @@ public class UnitOfWork : IUnitOfWork
         foreach (var domainEvent in domainEvents)
             await _mediator.Publish(domainEvent);
     }
+
+    private async Task SaveDomainEventsInOutboxEventsAsync()
+    {
+        // Достаем доменные события из Aggregate и преобразовываем их к OutboxMessage
+        var outboxMessages = _dbContext.ChangeTracker
+            .Entries<Aggregate>()
+            .Select(x => x.Entity)
+            .SelectMany(aggregate =>
+            {
+                var domainEvents = aggregate.GetDomainEvents();
+                aggregate.ClearDomainEvents();
+                return domainEvents;
+            }
+            )
+            .Select(domainEvent => new OutboxMessage
+            {
+                Id = domainEvent.EventId,
+                OccuredOnUtc = DateTime.UtcNow,
+                Type = domainEvent.GetType().Name,
+                Content = JsonConvert.SerializeObject(
+                    domainEvent,
+                    new JsonSerializerSettings
+                    {
+                        TypeNameHandling = TypeNameHandling.All
+                    })
+            })
+            .ToList();
+
+        // Добавяляем OutboxMessage в dbContext, после выхода из метода они и сам Aggregate будут сохранены
+        await _dbContext.Set<OutboxMessage>().AddRangeAsync(outboxMessages);
+    }
+
 }
